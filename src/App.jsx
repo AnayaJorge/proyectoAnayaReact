f0928117eefdf157851ce57ee9166b598a528071
Jorge Anaya <jeanaya@outlook.com>
2022-11-29 15:50:02 -0300

    iniciamos el proyecto

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>CODERHOUSE</h1>
+      <p>Curso de React JSS</p>
+      <p><img src="images/cgOficina150.jpg" alt="" /></p>
+    </div>
+    
+  );
+}
+
+export default App;
